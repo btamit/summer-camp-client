1f4b6c3259cb8aaf39b048ea3661084d59027bf5
btamit <bishojittoppo118@gmail.com>
2023-06-16 19:45:12 +0600

    stripe payment added

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import CheckoutForm from './CheckoutForm';
 import { loadStripe } from '@stripe/stripe-js';
 import {Elements} from "@stripe/react-stripe-js"
+import useCart from '../hooks/useCart';
 
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) =>sum + item.price,0);
+    const price = parseFloat(total.toFixed(2));
     return (
         <div>
             <h2 className='text-5xl font-bold mb-5 text-center'>Please Make Payment !!! </h2>
             <Elements stripe={stripePromise}>
-                <CheckoutForm></CheckoutForm>
+                <CheckoutForm price={price}></CheckoutForm>
             </Elements>
         </div>
     );
