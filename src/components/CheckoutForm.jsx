53617c1667ff69dbe4cc383f9d495afc90d5e96b
btamit <bishojittoppo118@gmail.com>
2023-06-16 20:21:43 +0600

    payment almost done

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,10 +1,25 @@
 import { useStripe,CardElement,useElements } from '@stripe/react-stripe-js';
 import React from 'react';
 import { useState } from "react";
+import { useEffect } from "react";
+import useAxiosSecure from '../hooks/useAxiosSecure';
+import useAuth from '../hooks/useAuth';
 const CheckoutForm = ({price}) => {
     const stripe = useStripe();
     const elements = useElements();
+    const {user} = useAuth();
+    const [axiosSecure] = useAxiosSecure();
     const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+
+    useEffect(() =>{
+      axiosSecure.post("/create-payment-intent",{price})
+      .then(res =>{
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret)
+      })
+    },[price, axiosSecure])
+
 
    const handleSubmit = async (event) =>{
         event.preventDefault();
@@ -27,6 +42,23 @@ const CheckoutForm = ({price}) => {
             setCardError('');
             console.log('payment method', paymentMethod)
         }
+         const { paymentIntent, error: confirmError } =
+           await stripe.confirmCardPayment(clientSecret, {
+             payment_method: {
+               card: card,
+               billing_details: {
+                 email: user?.email || "unknown",
+                 name: user?.displayName || "anonymous",
+               },
+             },
+           });
+
+           if(confirmError){
+            console.log(confirmError)
+           }
+           console.log(paymentIntent)
+
+
    }
     return (
       <>
@@ -50,7 +82,7 @@ const CheckoutForm = ({price}) => {
           <button
             className="btn btn-primary btn-sm mt-4"
             type="submit"
-            disabled={!stripe}
+            disabled={!stripe || !clientSecret}
           >
             Pay
           </button>
