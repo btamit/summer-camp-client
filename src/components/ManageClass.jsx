11f145b778221854f8a3f0c7640289bbd8cc64a0
btamit <bishojittoppo118@gmail.com>
2023-06-16 12:59:48 +0600

    manage class added

--- src/components/ManageClass.jsx
+++ src/components/ManageClass.jsx
@@ -1,30 +1,57 @@
 import React from 'react';
 import useClasses from '../hooks/useClasses';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../hooks/useAxiosSecure';
 
 const ManageClass = () => {
-    const[classes] = useClasses();
+    const[classes, ,refetch] = useClasses();
+    const [axiosSecure] = useAxiosSecure();
+    const handleDelete = (classItem) => {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axiosSecure.delete(`/classes/${classItem._id}`).then((res) => {
+            console.log("deleted res", res.data);
+            if (res.data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+        }
+      });
+    };
     return (
       <div>
-        <h2 className="text-5xl font-bold text-center">Manage Class : {classes.length}</h2>
+        <h2 className="text-5xl font-bold text-center">
+          Manage Class : {classes.length}
+        </h2>
         <div className="overflow-x-auto">
           <table className="table">
             {/* head */}
             <thead>
               <tr>
                 <th>#</th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-                <th>Update</th>
-                <th>Delete</th>
+                <th>Class Name & Image</th>
+                <th>Instructor Name</th>
+                <th>Instructor Email</th>
+                <th>Availabe Seats</th>
+                <th>Price</th>
+                <th>Pending</th>
+                <th>Approved</th>
+                <th>Denied</th>
               </tr>
             </thead>
             <tbody>
-              {classes.map((classItem,i) => (
+              {classes.map((classItem, i) => (
                 <tr key={classItem._id}>
-                  <td>
-                   {i+1}
-                  </td>
+                  <td>{i + 1}</td>
                   <td>
                     <div className="flex items-center space-x-3">
                       <div className="avatar">
@@ -36,19 +63,27 @@ const ManageClass = () => {
                         </div>
                       </div>
                       <div>
-                        <div className="font-bold">Hart Hagerty</div>
+                        <div className="font-bold">{classItem.className}</div>
                       </div>
                     </div>
                   </td>
+                  <td>{classItem.instructor}</td>
+                  <td>{classItem.email}</td>
+                  <td>{classItem.availableSeats}</td>
+                  <td className="text-right">$ {classItem.price}</td>
                   <td>
-                    Zemlak, 
+                    <button className="btn btn-ghost btn-xs">Pending</button>
                   </td>
-                  <td>Purple</td>
                   <td>
-                    <button className="btn btn-ghost btn-xs">details</button>
+                    <button className="btn btn-ghost btn-xs">Approved</button>
                   </td>
                   <td>
-                    <button className="btn btn-ghost btn-xs">details</button>
+                    <button
+                      onClick={() => handleDelete(classItem)}
+                      className="btn btn-ghost btn-xs"
+                    >
+                      Denied
+                    </button>
                   </td>
                 </tr>
               ))}
--- src/hooks/useClasses.jsx
+++ src/hooks/useClasses.jsx
@@ -15,14 +15,14 @@ const useClasses = () => {
   // }, []);
 
 
-  const {data: classes = [], isloading: loading} = useQuery({
+  const {data: classes = [], isloading: loading, refetch} = useQuery({
     queryKey:['classes'],
     queryFn: async() =>{
       const res = await fetch("http://localhost:5000/classes");
       return res.json();
     }
   })
-  return[classes,loading]
+  return[classes,loading,refetch]
 };
 
 export default useClasses;
